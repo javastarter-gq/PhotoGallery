35f30d8581f0d54cf7d399645b404d8d9f9071ba
Andrew <avpk77@gmail.com>
2016-05-21 23:27:31 +0300

    step_2

---
+++ src/main/java/gq/javastarters/Controllers/Controllers.java
@@ -0,0 +1,16 @@
+package gq.javastarters.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by a_pan on 21.05.2016.
+ */
+@Controller
+public class Controllers {
+
+    @RequestMapping("/photo")
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,5 @@
+<html>
+<p align="center"><br>Type your pic folder:</br> </p><p align="center"><input type= "text" name= "way"> </p>
+<p align="center">and create your gallery</p>
+<p align="center"><input type= "submit" value= "Create Gallery"></p>
+</html>
